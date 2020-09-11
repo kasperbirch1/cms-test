2b56795c0db60bdaf5ed5dea7f45d9a43583b76d
Kasper Aleksander Birch <6140244@RTS-365.dk>
2020-09-11 10:43:04 +0200

    updated

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,8 @@
 const path = require(`path`)
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
-    const { createPage } = actions
-    const result = await graphql(`
+  const { createPage } = actions
+  const result = await graphql(`
     {
       allMarkdownRemark {
         edges {
@@ -16,18 +16,18 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     }
   `)
 
-    if (result.errors) {
-        reporter.panicOnBuild(`Error while running GraphQL query.`)
-        return
-    }
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
 
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-        createPage({
-            path: node.frontmatter.path,
-            component: path.resolve(`src/templates/blogTemplate.js`),
-            context: {
-                path: node.frontmatter.path
-            }
-        })
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.path,
+      component: path.resolve(`src/templates/blogTemplate.js`),
+      context: {
+        path: node.frontmatter.path
+      }
     })
+  })
 }
